ce354810dfb3d6a88e21ce660abfa29e9ede86e1
icadfe <93196208+icadfe@users.noreply.github.com>
2021-12-15 21:41:53 +0700

    Update Irsyad.php

--- Irsyad.php
+++ Irsyad.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("auto_detect_line_endings", true);
-$data = file_get_contents('localhost/api/Irsyad.php');
+$data = file_get_contents('localhost/api/data.php');
 $decodedData = json_decode($data,true);
 var_dump($decodedData);
 ?>
