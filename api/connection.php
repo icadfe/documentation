ad65175da5f84dba403821b5e350d77fbadb63f7
icadfe <93196208+icadfe@users.noreply.github.com>
2021-12-16 00:04:53 +0700

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -3,7 +3,7 @@
 define('HOST','localhost');
 define('USER','root');
 define('PASS','');
-define('DB','epiz_30597276_student');
+define('DB','student');
 
 $connection=mysqli_connect(HOST,USER,PASS,DB) or die (
     'Unable to connect'
